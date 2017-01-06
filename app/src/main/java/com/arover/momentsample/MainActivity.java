e32e18e570c299bc6d1b6872a75100eaf38cf9f7
Arover <ryan.arover@gmail.com>
2017-01-06 17:15:51 +0800

    update project gradle. add some usage.

--- app/src/main/java/com/arover/momentsample/MainActivity.java
+++ app/src/main/java/com/arover/momentsample/MainActivity.java
@@ -15,18 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        StringBuilder builder = new StringBuilder();
         Moment moment = new Moment();
-        moment.edit().setDay(19);
+//        moment.edit().setDay(19);
 
         Moment m1 = new Moment();
         m1.edit().setDay(21);
 
-        Log.d(TAG,"now.format()="+moment.display().fromNow(this,m1));
-        Log.d(TAG,"first day of month="+moment.query().firstDayOfMonth());
-        Log.d(TAG,"last monday="+moment.query().lastMonday());
+        builder.append("Display: time from now=").append(moment.display().fromNow(this, m1)).append('\n');
+        builder.append("Display: date=").append(moment.display().date()).append('\n');
+        builder.append("Display: dateIso=").append(moment.display().dateIso()).append('\n');
+        builder.append("Display: formatIso8601=").append(moment.display().formatIso8601()).append('\n');
+        builder.append("Display: shortestDate=").append(moment.display().shortestDate()).append('\n');
+        builder.append("Display: simpleTime=").append(moment.display().simpleTime()).append('\n');
+        builder.append("Display: time=").append(moment.display().time()).append('\n');
+
+
+        builder.append("Query: is leap year=").append(moment.query().isLeapYear()).append('\n');
+        builder.append("Query: last monday=").append(moment.query().lastMonday()).append('\n');
+        builder.append("Query: first day of month=").append(moment.query().firstDayOfMonth()).append('\n');
+
         TextView demoText = (TextView) findViewById(R.id.text);
-        demoText.setText(moment.query().lastMonday().toString());
+        demoText.setText(builder.toString());
 
     }
 }
